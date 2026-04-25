f75a604f9d2a8ae06b3f2f6e12c24caf4f73be09
Mike <levinmk23@gmail.com>
2026-04-25 13:39:12 +0300

    lecture 3

--- src/main/java/hse/java/practice/chatstomp/ChatController.java
+++ src/main/java/hse/java/practice/chatstomp/ChatController.java
@@ -1,13 +1,7 @@
 package hse.java.practice.chatstomp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 public class ChatController {
@@ -16,5 +10,10 @@ public class ChatController {
     public String index() {
         return "chat";
     }
+
+
+//    @MessageMapping("/send")
+//    @SendTo("/topic/messages")
+//    your function
 
 }
--- src/main/java/hse/java/practice/chatstomp/websocket/WebSocketConfig.java
+++ src/main/java/hse/java/practice/chatstomp/websocket/WebSocketConfig.java
@@ -1,26 +1,23 @@
 package hse.java.practice.chatstomp.websocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
-/**
- * Spring конфигурация для WebSocket.
- *
- * @Configuration — Spring понимает, что это конфигурационный класс
- * @EnableWebSocket — активирует поддержку WebSocket в приложении
- *
- * registerWebSocketHandlers — регистрирует обработчик на определённом пути.
- * Клиент подключается к ws://localhost:8080/ws/chat
- */
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new ChatWebSocketHandler(), "/ws/chat")
-            .setAllowedOrigins("*");  // В продакшене ограничить до конкретного origin
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .withSockJS();
+    }
+
 }
